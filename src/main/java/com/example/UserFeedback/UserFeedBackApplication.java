52f2aea62e4731a8515453c4d7d68d90ce63241b
bukkesaikumarnaik <naik.sai333@gmail.com>
2022-09-22 23:20:55 +0530

    first commit

---
+++ src/main/java/com/example/UserFeedback/UserFeedBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.UserFeedback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserFeedBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserFeedBackApplication.class, args);
+	}
+
+}
